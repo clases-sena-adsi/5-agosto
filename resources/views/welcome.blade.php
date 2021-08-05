a473be07dd9b99d6556167e659aea6079a20f605
andrescortesdev <andrescortes.development@gmail.com>
2021-08-05 19:28:49 +0200

    Initial commit

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,6 @@
+<h1>Hola mundo</h1>
+<p>
+    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Amet dignissimos eaque nemo similique voluptatem. A
+    consequuntur eos ipsam laboriosam molestias sunt vel voluptatibus. Dicta enim mollitia nam similique totam
+    veritatis.
+</p>
